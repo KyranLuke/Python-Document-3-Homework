36fd10117e981d7c349f7336fcc4324f8e9b3c52
Kyran Truax <kyrantruax13@gmail.com>
2022-02-28 07:51:47 -0500

    Python Document 3 Homework

---
+++ my_import.py
@@ -0,0 +1,12 @@
+def area_house():
+    len_1 = input("Enter length ")
+    wid_1 = input("Enter Width ")
+    print(int(len_1) * int(wid_1))
+area_house()
+
+from math import pi as p
+
+def circumference_1():
+    radius_1 = input("Enter radius of circle ")
+    print(2 * p * float(radius_1))
+circumference_1()
